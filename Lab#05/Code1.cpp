88ebe7698c9d1d04bdd8074ccfa2ad65305d2e2b
Muhammad Tawha <tawhagenius@gmail.com>
2024-09-25 09:29:22 +0500

    added

---
+++ Lab#05/Code1.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+#define SIZE 5 
+class Queue
+
+{
+private:
+int items[SIZE];
+int front, rear;
+
+public:
+Queue() : front(-1), rear(-1) {}
+
+bool isEmpty() const {
+    return front == -1; 
+}
+
+bool isFull() const {
+    returnrear == SIZE - 1; 
+}
+void enqueue(int element) {
+    if (isFull()) {
+    cout << "Queue is full\n";
+}else{
+    if (isEmpty()) front = 0;
+    items[++rear] = element;
+    cout << "Inserted: " << element << endl; }
+}
+int dequeue() { 
+    if(isEmpty()) {
+    cout << "Queue is empty\n"; return -1; 
+}else{
+    int element = items[front]; 
+    if (front >= rear) {
+    front = rear = -1; 
+}else{
+    front++; }
+cout << "Deleted: " << element << endl;
+return element; }
+}
+void display() const { if(isEmpty()) {
+cout << "Queue is empty\n"; }else{
+cout << "Queue elements: "; f or(inti=front;i<=rear;++i){
+cout << items[i] << " "; }
+cout << endl; }
+} };
+int main() {
+Queue q; 
+q.dequeue();
+q.enqueue(10); q.enqueue(20); q.enqueue(30); q.enqueue(40); q.enqueue(50);
+return 0; }
